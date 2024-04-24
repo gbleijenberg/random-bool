6a6907834f34a86f2286d31927dd3f37cf31558d
gbleijenberg <gbleijenberg@schueler.hakzell.at>
2024-04-24 12:14:47 +0200

    add function

--- src/Rand.php
+++ src/Rand.php
@@ -12,6 +12,16 @@ class Rand
     {
         $gen = rand(0, 1);
 
+        if ($gen == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
         // Überprüfung des Datentyps bool mit 3 x =; es wird entweder true zurückgegeben wenn $gen
         // 1 ist oder false wenn $gen 0 ist
         return $gen === 1;
